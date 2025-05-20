5738e0a89b22bd814feaaa0187a3508dde4c1f4d
Monalika Patnaik <monalikapatnaik114@gmail.com>
2025-05-21 03:39:02 +0530

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { PostHogProvider } from 'posthog-js/react'
-
+import { Analytics } from "@vercel/analytics/react"
 const options = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
 }
@@ -12,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
     apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} 
     options={options}
   >
+    <Analytics />
     <App />
   </PostHogProvider>
 );
